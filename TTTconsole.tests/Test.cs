4611f4dc408e1e78fc6527c1487a7b9d325d2e46
Raymond T. Hightower <rth@wisdomgroup.com>
2016-11-06 20:37:31 -0600

    Initial commit.

---
+++ TTTconsole.tests/Test.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using TTTconsole;
+
+namespace TTTconsole.tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void MustPass()
+		{
+			int expected = 1;
+			int actual = 1;
+			Assert.AreEqual(expected, actual);
+		}
+
+		public void CreateTTTBoard()
+		{
+			char[] ExpectedGrid = new char[10];
+			for (int i = 1; i < 10; i++)
+			{
+				ExpectedGrid[i] = (char)(i + 48);
+			}
+
+			char[] ActualGrid = LoadGrid();
+
+			Assert.AreSame(ExpectedGrid, ActualGrid);
+		}
+	}
+}
---
+++ TTTconsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TTTconsole
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+		static char[] LoadGrid()
+		{
+			for (int i = 1; i < 10; i++)
+			{		
+				grid[i] = (char)(i + 48);
+			}
+			return grid;
+		}
+	}
+}
